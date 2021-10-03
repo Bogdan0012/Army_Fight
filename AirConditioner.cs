b7163e8602322a42b6e67ab2efce864e2dfe2b5e
Bogdan Gamera <BodyaGamera@gmail.com>
2021-10-03 20:44:26 +0300

    -add class AirConditioner

---
+++ AirConditioner.cs
@@ -0,0 +1,35 @@
+namespace CProject
+{
+using System;    
+    class AirConditioner
+    {
+        short electric_power;
+
+        C_AirConditioner() {
+            this.electric_power = 1;
+        }
+        C_AirConditioner(short electric_power) {
+            this.electric_power = electric_power;
+        }
+        
+        public short ElectricityCheck() {
+            if (electric_power > 0){
+                Console.WriteLine("Power supply is normal,");
+            }
+            else{ 
+                Console.WriteLine("No power supply."); return 0;
+            }
+        }
+        
+        public void ON() {
+            Console.WriteLine("Air conditioner ON");
+        }
+        public void OFF() {
+            Console.WriteLine("Air conditioner OFF");
+        }
+        
+        public int TemperatureDrop(int temperature) {
+            return --temperature;
+        }
+    }
+}
